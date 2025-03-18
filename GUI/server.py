edcad7c3b6ea23d9db99af4bee4c353a4ba5ddbd
Ting-Jun Wang <levi900227@gmail.com>
2025-03-19 01:05:56 +0800

    feat: GUI animation show 3D accelerator values on STM32

--- GUI/server.py
+++ GUI/server.py
@@ -1,8 +1,21 @@
 import socket
 import struct
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import os
 
+# setup GUI
+plt.ion()
+fig, ax = plt.subplots(figsize=(6, 4))
+labels = ['X', 'Y', 'Z']
+bars = ax.bar(labels, [0, 0, 0], color=['r', 'g', 'b'])
+ax.set_ylim(-1500, 1500)
+ax.set_title('STM32 3D Accelerator Data')
+ax.set_xlabel('Axis')
+ax.set_ylabel('Value')
+ax.grid(axis='y', linestyle='--', alpha=0.7)
+
+
+# setup socket
 HOST = os.environ['STM32_SERVER_HOST']
 PORT = int(os.environ['STM32_SERVER_PORT'])
 
@@ -20,8 +33,20 @@ while True:
         try:
             data = client_sock.recv(6)
             values = struct.unpack('<hhh', data)
-            if data:
-                print(values)
+            print(values)
+
+            for bar, value in zip(bars, values):
+                bar.set_height(value)
+
+            for text in ax.texts:
+                text.remove()
+
+            for i, v in enumerate(values):
+                ax.text(i, v+5, str(v), ha='center', fontsize=12, color='black', weight='bold')
+
+            plt.draw()
+            plt.pause(0.001)
+
         except:
             client_sock.close()
             print("closed server")
